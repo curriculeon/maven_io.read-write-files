d91ba6226d152cee65144ab94a57762f48d5195e
santa-js-html <sanafaris2000@yahoo.com>
2020-01-09 15:57:00 -0500

    Updated

--- src/main/java/com/github/curriculeon/Document.java
+++ src/main/java/com/github/curriculeon/Document.java
@@ -1,62 +1,94 @@
 package com.github.curriculeon;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author leon on 16/11/2018.
  */
 public class Document implements DocumentInterface {
 
-    private final FileWriter fileWriter;
+    private FileWriter fileWriter;
+    private final String fileNameForTest;
     private final File file;
+    private final Reader reader;
+    private Scanner scanner;
+    private String contents;
+    private List<String> stringList;
+
+//    public Document(String fileName) throws IOException {
+//        this(new File(fileName));
+//    }
 
     public Document(String fileName) throws IOException {
-        this(new File(fileName));
-    }
-
-    public Document(File file) throws IOException {
-        this.file = file;
-        this.fileWriter = new FileWriter(file);
-    }
-
-    @Override
-    public void write(String contentToBeWritten) {
+        this.file = new File(fileName);
+        this.fileWriter = new FileWriter(getFile());
+        this.reader = new FileReader(getFile());
+        this.scanner = new Scanner(this.reader);
+        this.fileNameForTest = fileName;
     }
 
     @Override
-    public void write(Integer lineNumber, String valueToBeWritten) {
+    public void write(String contentToBeWritten) throws IOException{
+        this.fileWriter.write(contentToBeWritten);
+        this.fileWriter.close();
     }
 
     @Override
-    public String read(Integer lineNumber) {
-        return null;
+    public void write(Integer lineNumber, String valueToBeWritten) throws IOException {
+        toList().set(lineNumber, valueToBeWritten);
+        this.fileWriter.write(toString());
+        this.fileWriter.close();
     }
 
     @Override
-    public String read() {
-        return null;
+    public String read(Integer lineNumber) throws IOException {
+        return this.toList().get(lineNumber);
     }
 
     @Override
-    public void replaceAll(String stringToReplace, String replacementString) {
+    public String read()  {
+        this.contents = scanner.nextLine();
+        while(scanner.hasNextLine()){
+            this.contents += '\n'+ scanner.nextLine();
+        }
+        try {
+            reader.close();
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+        return this.contents;
     }
 
     @Override
-    public void overWrite(String content) {
-    }
-
-    public List<String> toList() {
-        return null;
+    public void replaceAll(String stringToReplace, String replacementString) throws IOException {
+        this.contents = read().replaceAll(stringToReplace, replacementString);
+        this.fileWriter.write(this.contents);
+        this.fileWriter.close();
     }
 
     @Override
-    public File getFile() {
-        return null;
+    public void overWrite(String content) throws IOException {
+        this.fileWriter = new FileWriter(getFile(), false);
+        this.fileWriter.write(content);
+        this.fileWriter.close();
+    }
+
+    public List<String> toList() throws IOException {
+        this.stringList = Arrays.asList(read().split("\n"));
+        return this.stringList;
+    }
+
+    @Override
+    public File getFile() throws FileNotFoundException {
+        return this.file;
     }
 
     @Override
     public String toString() {
-        return null;
+        read();
+     return fileNameForTest+'{'+this.contents+'}';
     }
 }
--- src/main/java/com/github/curriculeon/DocumentInterface.java
+++ src/main/java/com/github/curriculeon/DocumentInterface.java
@@ -1,27 +1,29 @@
 package com.github.curriculeon;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * @author leon on 16/11/2018.
  */
 public interface DocumentInterface {
-    void write(String contentToBeWritten);
+    void write(String contentToBeWritten) throws IOException;
 
-    void write(Integer lineNumber, String valueToBeWritten);
+    void write(Integer lineNumber, String valueToBeWritten) throws IOException;
 
-    String read(Integer lineNumber);
+    String read(Integer lineNumber) throws IOException;
 
-    String read();
+    String read() throws IOException;
 
-    void replaceAll(String stringToReplace, String replacementString);
+    void replaceAll(String stringToReplace, String replacementString) throws IOException;
 
-    void overWrite(String content);
+    void overWrite(String content) throws IOException;
 
-    List<String> toList();
+    List<String> toList() throws IOException;
 
-    File getFile();
+    File getFile() throws FileNotFoundException;
 
     @Override
     String toString();
