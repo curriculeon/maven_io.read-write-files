4c633132914663721252bb0dff66c00fa3307699
santa-js-html <sanafaris2000@yahoo.com>
2020-01-13 11:36:45 -0500

    Done 1/13/20

--- src/main/java/com/github/curriculeon/Document.java
+++ src/main/java/com/github/curriculeon/Document.java
@@ -1,5 +1,6 @@
 package com.github.curriculeon;
 
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
@@ -13,76 +14,85 @@ public class Document implements DocumentInterface {
     private FileWriter fileWriter;
     private final String fileNameForTest;
     private final File file;
-    private final Reader reader;
     private Scanner scanner;
-    private String contents;
+    String contents;
     private List<String> stringList;
-
-//    public Document(String fileName) throws IOException {
-//        this(new File(fileName));
-//    }
 
     public Document(String fileName) throws IOException {
         this.file = new File(fileName);
-        this.fileWriter = new FileWriter(getFile());
-        this.reader = new FileReader(getFile());
-        this.scanner = new Scanner(this.reader);
         this.fileNameForTest = fileName;
     }
 
     @Override
-    public void write(String contentToBeWritten) throws IOException{
-        this.fileWriter.write(contentToBeWritten);
-        this.fileWriter.close();
+    public void write(String contentToBeWritten){
+        try {
+            this.fileWriter = new FileWriter(getFile());
+            this.fileWriter.write(contentToBeWritten);
+            this.fileWriter.flush();
+            this.fileWriter.close();
+        }catch (IOException e){
+            throw new Error(e);
+        }
     }
 
     @Override
-    public void write(Integer lineNumber, String valueToBeWritten) throws IOException {
-        toList().set(lineNumber, valueToBeWritten);
-        this.fileWriter.write(toString());
-        this.fileWriter.close();
+    public void write(Integer lineNumber, String valueToBeWritten){
+        List<String> writeToLine = toList();
+        writeToLine.set(lineNumber, valueToBeWritten);
+        String writtenLine = new String();
+        for(String i:writeToLine){
+           writtenLine+=i +'\n';
+        }
+        write(writtenLine);
     }
 
     @Override
-    public String read(Integer lineNumber) throws IOException {
+    public String read(Integer lineNumber){
         return this.toList().get(lineNumber);
     }
 
     @Override
     public String read()  {
-        this.contents = scanner.nextLine();
-        while(scanner.hasNextLine()){
-            this.contents += '\n'+ scanner.nextLine();
-        }
         try {
-            reader.close();
+            this.scanner = new Scanner(this.file);
+            this.contents = new String();
+            while (scanner.hasNextLine()) {
+                this.contents += scanner.nextLine();
+                if(scanner.hasNextLine()) this.contents+='\n';
+            }
+
+        }catch (IOException e){
+            throw new Error(e);
+        }
+               return this.contents;
+    }
+
+    @Override
+    public void replaceAll(String stringToReplace, String replacementString){
+        String replacedContents = read();
+        replacedContents = replacedContents.replaceAll(stringToReplace, replacementString);
+        overWrite(replacedContents);
+    }
+
+    @Override
+    public void overWrite(String content){
+        try {
+            FileWriter overWriter = new FileWriter(getFile(), false);
+            overWriter.write(content);
+            overWriter.flush();
+            overWriter.close();
         } catch (IOException e) {
             throw new Error(e);
         }
-        return this.contents;
     }
 
-    @Override
-    public void replaceAll(String stringToReplace, String replacementString) throws IOException {
-        this.contents = read().replaceAll(stringToReplace, replacementString);
-        this.fileWriter.write(this.contents);
-        this.fileWriter.close();
-    }
-
-    @Override
-    public void overWrite(String content) throws IOException {
-        this.fileWriter = new FileWriter(getFile(), false);
-        this.fileWriter.write(content);
-        this.fileWriter.close();
-    }
-
-    public List<String> toList() throws IOException {
+    public List<String> toList(){
         this.stringList = Arrays.asList(read().split("\n"));
         return this.stringList;
     }
 
     @Override
-    public File getFile() throws FileNotFoundException {
+    public File getFile(){
         return this.file;
     }
 
