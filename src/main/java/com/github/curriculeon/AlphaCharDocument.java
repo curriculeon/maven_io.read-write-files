32183116c75f467d66569e21876f1fd0037b301a
dmholland <kenoshatrader@gmail.com>
2020-08-11 19:22:46 -0400

    Done

--- src/main/java/com/github/curriculeon/AlphaCharDocument.java
+++ src/main/java/com/github/curriculeon/AlphaCharDocument.java
@@ -1,5 +1,9 @@
 package com.github.curriculeon;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -8,13 +12,18 @@ import java.io.IOException;
 public class AlphaCharDocument extends Document {
     public AlphaCharDocument(String fileName) throws IOException {
         super(fileName);
+
     }
 
+
     @Override
-    public void write(String contentToBeWritten) {
+    public void write(String contentToBeWritten) throws IOException  {
+        if(!isAlpha(contentToBeWritten)){throw new IllegalArgumentException();}
+        super.write(contentToBeWritten);
     }
 
     private Boolean isAlpha(String s) {
-        return null;
+
+        return s.matches("[a-z][A-Z]+");
     }
 }
--- src/main/java/com/github/curriculeon/Document.java
+++ src/main/java/com/github/curriculeon/Document.java
@@ -1,7 +1,10 @@
 package com.github.curriculeon;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author leon on 16/11/2018.
@@ -9,6 +12,11 @@ import java.util.List;
 public class Document implements DocumentInterface {
 
     private final FileWriter fileWriter;
+
+    public FileWriter getFileWriter() {
+        return fileWriter;
+    }
+
     private final File file;
 
     public Document(String fileName) throws IOException {
@@ -21,7 +29,14 @@ public class Document implements DocumentInterface {
     }
 
     @Override
-    public void write(String contentToBeWritten) {
+    public void write(String contentToBeWritten) throws IOException {
+        try {
+            this.fileWriter.write(contentToBeWritten);
+        } catch (IOException e) {
+            System.out.println("Filewriter can't write to " + file);
+        } finally {
+            fileWriter.flush();
+        }
     }
 
     @Override
@@ -29,34 +44,55 @@ public class Document implements DocumentInterface {
     }
 
     @Override
-    public String read(Integer lineNumber) {
+    public String read(Integer lineNumber) throws IOException {
+  List<String> list=this.toList();
+        return list.get(lineNumber);//Originally used buffered reader, but I decided to go with the list method
+    }
+
+    @Override
+    public String read() throws IOException {
+        FileReader fileReader = new FileReader(file);
+        try {
+            char[] chars = new char[(int) file.length()];
+            fileReader.read(chars);
+            return String.valueOf(chars);
+        } catch (IOException e) {
+            System.out.println("Filewriter can't write to " + file);
+        }
         return null;
     }
 
     @Override
-    public String read() {
-        return null;
+    public void replaceAll(String stringToReplace, String replacementString) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
+        content = content.replaceAll(stringToReplace, replacementString);
+        Files.write(Paths.get(file.getPath()), content.getBytes());
     }
 
     @Override
-    public void replaceAll(String stringToReplace, String replacementString) {
+    public void overWrite(String content) throws IOException {
+
+        replaceAll(this.read(), content);
     }
 
-    @Override
-    public void overWrite(String content) {
-    }
 
-    public List<String> toList() {
-        return null;
+    public List<String> toList() throws IOException {
+        return Files.lines(Paths.get(file.getPath())).collect(Collectors.toList());  //Got from Chris Fulton
     }
 
     @Override
     public File getFile() {
-        return null;
+        return this.file;
     }
 
     @Override
     public String toString() {
+        try {
+            return file.toString()+"{"+this.read()+"}";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
+
